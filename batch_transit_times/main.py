28ccdb4c1e67c96eb91126613aa7bd3e3c58fea8
christopherpryer <christophpryer@gmail.com>
2019-09-26 17:02:47 -0400

    completed basic transit time request feature

--- batch_transit_times/main.py
+++ batch_transit_times/main.py
@@ -1,3 +1,61 @@
+from fedex.services.availability_commitment_service import \
+    FedexAvailabilityCommitmentRequest
+from fedex.tools.conversion import sobject_to_dict
+import pandas as pd
+import numpy as np
+import logging
+import sys
+
+
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+
 class PandasWrapper:
-    def __init__(self):
-        pass
+    def __init__(self, config):
+        self.df = pd.DataFrame()
+        self.config = config
+
+    def get_transit_time(self, origin_zip, origin_country, dest_zip,
+        dest_country):
+        self.request = FedexAvailabilityCommitmentRequest(self.config)
+        self.request.Origin.PostalCode = origin_zip
+        self.request.Origin.CountryCode = origin_country
+        self.request.Destination.PostalCode = dest_zip
+        self.request.Destination.CountryCode = dest_country
+        self.request.Service = 'FEDEX_GROUND'
+
+        self.request.send_request()
+        response_dict = sobject_to_dict(self.request.response)
+
+        # output display formatting
+        origin_str = '%s, %s' % (
+            self.request.Origin.PostalCode,
+            self.request.Origin.CountryCode)
+        destination_str = '%s, %s' % (
+            self.request.Destination.PostalCode,
+            self.request.Destination.CountryCode)
+
+        logging.info('origin: %s' % origin_str)
+        logging.info('destination: %s' % destination_str)
+        for option in response_dict['Options']:
+            if option['Service'] == 'FEDEX_GROUND':
+                logging.info('TransitTime: %s' % option['TransitTime'])
+                return option['TransitTime'] # TODO: convert from type to int
+            else:
+                logging.warning('No Fedex Ground Service found.')
+                return np.nan
+
+    def run(self):
+        if not self.df.empty:
+            transit_times = []
+            for i in range(len(self.df)):
+                o_zip = self.df.origin_zip.iloc[i]
+                o_country = self.df.origin_country.iloc[i]
+                d_zip = self.df.dest_zip.iloc[i]
+                d_country = self.df.dest_country.iloc[i]
+                tt = self.get_transit_time(
+                    origin_zip=o_zip,
+                    origin_country=o_country,
+                    dest_zip=d_zip,
+                    dest_country=d_country)
+                transit_times.append(tt)
+            self.df['transit_time'] = transit_times
--- tests.py
+++ tests.py
@@ -1,15 +1,37 @@
+from fedex_config import CONFIG_OBJ
 from batch_transit_times import PandasWrapper
 import logging
 import sys
+import os
+import pandas as pd
 
 
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
 def test_init():
-    helper = PandasWrapper()
+    helper = PandasWrapper(CONFIG_OBJ)
     assert helper
+
+def test_batch():
+    root_dir = os.path.dirname(os.path.abspath(__name__))
+    df = pd.read_csv(os.path.join(root_dir, 'testing_data.csv'))
+    assert not df.empty
+
+    # 7981 -> 07981
+    df.origin_zip = df.origin_zip.astype('str').str.zfill(5)
+    df.dest_zip = df.dest_zip.astype('str').str.zfill(5)
+
+    helper = PandasWrapper(CONFIG_OBJ)
+    helper.df = df.copy()
+    assert not helper.df.empty
+
+    helper.run()
+    assert 'transit_time' in helper.df.columns
+
+    logging.info('transit_times: %s' % helper.df.transit_time)
 
 if __name__ == '__main__':
     logging.info('initializing tests.')
     test_init()
+    test_batch()
     logging.info('completed tests.')
